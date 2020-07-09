cf67faee4f2fbe39027c0ef2dbd05900f4366b2a
What-a-mess <44860987+What-a-mess@users.noreply.github.com>
2020-07-09 19:14:18 +0800

    调整骨架

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,30 @@
 <template>
-    <div>
-    <TopBar />
-        <br /><br />
-    <LeftMenu />
-
-    </div>
+  <div>
+    <el-container>
+      <el-top>
+        <TopBar />
+      </el-top>
+      <el-container>
+        <el-aside>
+          <br/>
+          <LeftMenu />
+        </el-aside>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
-    import TopBar from "./TopAndLeft/TopBar";
-    import LeftMenu from "./TopAndLeft/LeftMenu";
-    export default {
-        name: "Main",
-        components: {LeftMenu, TopBar},
-    }
+import TopBar from "./TopAndLeft/TopBar";
+import LeftMenu from "./TopAndLeft/LeftMenu";
+export default {
+  name: "Main",
+  components: { LeftMenu, TopBar }
+};
 </script>
 
 <style scoped>
-
 </style>
