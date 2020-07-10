0e351fcfdbc9c5f6bb8a281fdb3a9fb923450de0
What-a-mess <44860987+What-a-mess@users.noreply.github.com>
2020-07-10 11:23:23 +0800

    新增买家信息展示以及供应商信息展示

---
+++ src/components/ClienteleMgmt/CustomerInfo.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-row>
+    <el-col>
+      <el-card>
+        <el-table :data="customers">
+          <el-table-column label="ID" prop="customerId"></el-table-column>
+          <el-table-column label="姓名" prop="customerName"></el-table-column>
+          <el-table-column label="电话" prop="customerPhone"></el-table-column>
+          <el-table-column label="邮箱" prop="customerEmail"></el-table-column>
+          <el-table-column label="地址" prop="customerAddress"></el-table-column>
+        </el-table>
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      customers: [
+        {
+          customerId: 93633694,
+          customerName: "声记小组",
+          customerPhone: "18189486539",
+          customerEmail: "k.bqvludanf@qq.com",
+          customerAddress: "海南省玉树藏族自治州鄂托克前旗"
+        },
+        {
+          customerId: -54997737,
+          customerName: "性格身候目本或",
+          customerPhone: "19835913636",
+          customerEmail: "g.lihnabwx@qq.com",
+          customerAddress: "澳门特别行政区平顶山市东至县"
+        },
+        {
+          customerId: -59559654,
+          customerName: "第些当类参养",
+          customerPhone: "18123858686",
+          customerEmail: "q.xyibjmjazy@qq.com",
+          customerAddress: "青海省温州市石景山区"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.el-col {
+    padding: 20px;
+}
+</style>
--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,19 +1,26 @@
 import Vue from 'vue'
-import  VueRouter from 'vue-router'
+import VueRouter from 'vue-router'
 import login from "../components/Login/login";
 import OrderFormInfo from "../components/OrderFormMgmt/OrderFormInfo";
 import ReturnFromInfo from "../components/OrderFormMgmt/ReturnFromInfo"
 import Main from "../components/Main";
 import OrderFormDetails from '../components/OrderFormMgmt/OrderFormDetails'
 
+// ClienteleMgmt
+import CustomerInfo from '../components/ClienteleMgmt/CustomerInfo'
+import SupplierInfo from '../components/ClienteleMgmt/SupplierInfo'
+
 const routes = [
     { path: '/', components: { default: login } },
-    { path: '/main', component: Main,
+    {
+        path: '/main', component: Main,
         children: [
             { path: '', component: OrderFormInfo },
             { path: 'returnFrom', component: ReturnFromInfo },
             { path: 'orderFrom', component: OrderFormInfo },
-            { path: 'order/:orderId/details', component:OrderFormDetails }
+            { path: 'order/:orderId/details', component: OrderFormDetails },
+            { path: 'customers', component: CustomerInfo },
+            { path: 'suppliers', component: SupplierInfo }
         ]
     }
 ]
